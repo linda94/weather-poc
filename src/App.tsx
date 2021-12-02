0a1dfc8084b89679b1592f93068fff506fd42352
Linda Tran <linda_tran@hotmail.com>
2021-12-02 23:04:11 +0100

    fix: add swr config and some styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import { SWRConfig } from 'swr';
 
 import './App.css';
 import TopBar from './components/TopBar';
@@ -7,15 +8,22 @@ import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <TopBar />
-      <main>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-        </Routes>
-      </main>
-    </div>
+    <SWRConfig
+      value={{
+        refreshInterval: 0,
+        fetcher: (resource, init) =>
+          fetch(resource, init).then((res) => res.json()),
+      }}>
+      <div className="App">
+        <TopBar />
+        <main>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Routes>
+        </main>
+      </div>
+    </SWRConfig>
   );
 }
 
